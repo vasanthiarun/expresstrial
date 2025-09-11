b2de1e968b53878d1e28f590723297b1842ea1c9
maria <maria@gsmhub.me>
2025-09-11 10:54:47 +0530

    db connection established, /users route added to list the users

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
+const mysql = require('mysql');
 
 // Initialize Express application
 const app = express();
 
 
+// MySQL connection setup
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // Add your MySQL password
+    database: 'crud_example'
+});
+
+// Connect to MySQL
+db.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL connected...');
+});
+
 // Define route handler for GET requests to '/'
 app.get('/', (req, res) => {
     res.send('Hello World\n');
+});
+
+
+//list users
+app.get('/users',  (request, response) =>{
+	const sql = 'select name, email from users';
+	db.query(sql, (err, records) => {
+		if(err)
+			response.send(err.code);
+		response.status(201).send(records);
+	});
+
 });
 
 // Start the server on port 5000
